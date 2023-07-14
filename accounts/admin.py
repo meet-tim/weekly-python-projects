0cb2e72a2f77f96d5bfa589497d3d2e7c3152dc9
meet-tim <53443019+meet-tim@users.noreply.github.com>
2023-07-14 18:47:16 +0000

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Owner,House,Contact
-# Register your models here.
-admin.site.register(Owner)
-admin.site.register(House)
-admin.site.register(Contact)
