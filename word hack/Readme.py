f3bd5447200810574e62579867f61263e1010245
meet-tim <53443019+meet-tim@users.noreply.github.com>
2023-07-14 18:12:33 +0000

    Create Readme.md

---
+++ word hack/Readme.py
@@ -0,0 +1,5 @@
+## This program provides hints to a word game. More like a game cheat
+I got frustrated using all my lifelines in aword game i used to play.
+1 So i decided to build a cheat program to generate the word using the given words. 
+
+pip insall nltk to use this program successfully
